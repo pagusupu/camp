b16a665424c785138d2318d68259f99567dbe6bc
pagusupu <me@pagu.cafe>
2023-08-31 17:28:40 +1200

    update waybar

--- things/home/waybar.nix
+++ things/home/waybar.nix
@@ -1,6 +1,5 @@
 {
   pkgs,
-  mcolours,
   config,
   lib,
   ...
@@ -22,20 +21,20 @@
           layer = "top";
           position = "top";
           output = ["DP-3" "HDMI-A-1"];
-          modules-left = ["clock"];
+          modules-left = ["custom/icon" "clock"];
           modules-center = ["hyprland/workspaces"];
           modules-right = ["wireplumber" "bluetooth" "network" "custom/powermenu"];
           "hyprland/workspaces" = {
             format = "{icon}";
             format-icons = {
-              "1" = "<span color='#8A98AC'>壹</span>";
-              "2" = "<span color='#8F8AAC'>貳</span>";
-              "3" = "<span color='#AC8AAC'>参</span>";
-              "4" = "<span color='#AC8A8C'>肆</span>";
-              "5" = "<span color='#8A98AC'>伍</span>";
-              "6" = "<span color='#8F8AAC'>陸</span>";
-              "7" = "<span color='#AC8AAC'>柒</span>";
-              "8" = "<span color='#AC8A8C'>捌</span>";
+              "1" = "一";
+              "2" = "二";
+              "3" = "三";
+              "4" = "四";
+              "5" = "五";
+              "6" = "六";
+              "7" = "七";
+              "8" = "八";
             };
             persistent_workspaces = {
               "*" = 4;
@@ -70,7 +69,7 @@
           };
           "custom/powermenu" = {
             format = "<span font_size='16pt'></span>";
-            on-click = "sleep 0.1 && /etc/nixos/things/scripts/powermenu.sh";
+            on-click = "sleep 0.1 && ~/Nix/things/scripts/powermenu.sh";
             tooltip = false;
           };
           "bluetooth" = {
@@ -80,7 +79,10 @@
              tooltip-format-enumerated-connected-battery = "{device_alias} - {device_battery_percentage}%: {device_address}";
              tooltip-format = "{controller_alias}: {status}";
           };
-          height = 30;
+	  "custom/icon" = {
+	    format = "<span font_size='16pt'></span>";
+	  };
+          height = 35;
           spacing = 4;
           margin-top = 10;
           margin-bottom = 0;
@@ -94,48 +96,51 @@
           min-height: 0;
           margin: 0;
           padding: 0;
-          font-family: "Nunito", "la-regular-lg", "Kosugi Maru";
+          font-family: "Nunito", "Line Awesome Free", "Kosugi Maru";
           font-size: 16px;
         }
         window#waybar {
           background: #191919;
-          border-radius: 5px;
+          border-radius: 10px;
         }
+	#workspaces {
+	  margin: 4px;
+	} 
         #workspaces button {
-          padding-top: 2px;
-          padding-bottom: 2px;
+          padding-top: 0px;
+          padding-bottom: 0px;
           padding-left: 2px;
           padding-right: 2px;
+	  color: #a0a0a0;
+	  background-color: #393939;
+	  border-radius: 6px;
+	  margin: 2px;
         }
         #workspaces button.active {
-          background: transparent;
-          border-bottom: 2px solid #bfbfbf;
-          border-radius: 0px;
+          background-color: #ac8aac;
+	  color: #191919;
+          border-radius: 6px;
         }
         #workspaces button:hover {
-          background: transparent;
+          background-color: #ac8aac;
+	  color: #191919;
           box-shadow: inherit;
           text-shadow: inherit;
-          border-bottom: 2px solid #a0a0a0;
-          border-radius: 0px;
+          border-radius: 6px;
         }
         #clock {
           color: #a0a0a0;
-          padding-top: 2px;
-          padding-bottom: 2px;
-          padding-right: 3px;
-          padding-left: 6px;
+          padding-top: 0px;
+          padding-bottom: 0px;
+          padding-right: 0px;
+          padding-left: 0px;
         }
-	#custom-icon {
-	  color: #a0a0a0;
-	  padding-left: 2px;
-	}
-        .modules-right {
+	.modules-right {
           color: #a0a0a0;
           padding-left: 0px;
-          padding-right: 0px;
-          padding-top: 2px;
-          padding-bottom: 2px;
+          padding-right: 2px;
+          padding-top: 0px;
+          padding-bottom: 0px;
         }
         #custom-powermenu {
 	  padding-right: 4px;
@@ -143,11 +148,15 @@
 	#network {
           padding-right: 2px;
         }
+	#custom-icon {
+	  padding-left: 4px;
+	  color: #a0a0a0;
+	}
         tooltip {
           background: #191919;
           color: #a0a0a0;
           border: 1px solid #a0a0a0;
-          border-radius: 7px;
+          border-radius: 6px;
         }
         tooltip label {
           color: #a0a0a0;
