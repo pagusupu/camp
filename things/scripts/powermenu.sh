26e6b64d8146a548aac91f707c04d7d4c5f46b02
pagusupu <me@pagu.cafe>
2023-08-31 15:05:56 +1200

    update powermenu

--- things/scripts/powermenu.sh
+++ things/scripts/powermenu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-op=$( echo -e " Poweroff\n Reboot\n Lock\n Logout" | tofi | awk '{print tolower($2)}' )
+op=$( echo -e " Poweroff\n Reboot\n Lock\n Logout" | tofi | awk '{print tolower($2)}' )
 
 case $op in 
         poweroff)
