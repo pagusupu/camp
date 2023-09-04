f00d71429beeda74a3ec99db5fa340790dcd4f4e
pagusupu <me@pagu.cafe>
2023-09-04 15:29:20 +1200

    codium

--- flake.nix
+++ flake.nix
@@ -1,14 +1,10 @@
-{ 
+{
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     hyprland.url = "github:hyprwm/Hyprland";
-    alejandra.url = "github:kamadorueda/alejandra/3.0.0";
   };
-  outputs = {
-    nixpkgs,
-    ...
-  } @ inputs: {
+  outputs = {nixpkgs, ...} @ inputs: {
     nixosConfigurations = {
       home = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -21,5 +17,3 @@
     };
   };
 }
-         
-                
