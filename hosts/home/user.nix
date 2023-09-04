eecdab0fcb05ce28426828c05864d1b030d80350
pagusupu <me@pagu.cafe>
2023-09-04 16:01:24 +1200

    codium

--- hosts/home/user.nix
+++ hosts/home/user.nix
@@ -79,7 +79,24 @@
           mutableExtensionsDir = false;
           extensions = with pkgs.vscode-extensions; [
             kamadorueda.alejandra
+            oderwat.indent-rainbow
+            vscode-icons-team.vscode-icons
           ];
+          userSettings = {
+            "terminal.external.linuxExec" = "alacritty";
+            "alejandra.program" = "alejandra";
+	    "[nix]" = {
+              "editor.defaultFormatter" = "kamadorueda.alejandra";
+              "editor.formatOnPaste" = true;
+              "editor.formatOnSave" = true;
+              "editor.formatOnType" = true;
+            };
+            "editor.fontFamily" = "'firacode', 'monospace', monospace";
+            "git.enableSmartCommit" = true;
+            "git.confirmSync" = false;
+            "window.menuBarVisibility" = "toggle";
+            "workbench.iconTheme" = "vscode-icons";
+          };
         };
       };
     };
