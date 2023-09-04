5b9657dfb37e8cf2457ea55fc445841fc75cd0fb
pagusupu <me@pagu.cafe>
2023-09-04 17:39:31 +1200

    cleanup

--- hosts/home/hardware.nix
+++ hosts/home/hardware.nix
@@ -1,18 +1,18 @@
-{pkgs, ... }: {
+{pkgs, ...}: {
   boot = {
     kernelPackages = pkgs.linuxPackages_xanmod_latest;
     loader = {
       grub = {
         enable = true;
-	useOSProber = true;
-	efiSupport = true;
-	device = "nodev";
-      };      
-     efi.canTouchEfiVariables = true;
+        useOSProber = true;
+        efiSupport = true;
+        device = "nodev";
+      };
+      efi.canTouchEfiVariables = true;
     };
     initrd = {
       kernelModules = ["amdgpu"];
-      availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+      availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
     };
     kernelParams = [
       "video=HDMI-A-1:1920x1080@75"
@@ -20,7 +20,7 @@
       "amd_pstate=passive"
       "amd_pstate.shared_mem=1"
     ];
-    kernelModules = [ "kvm-amd" "amd_pstate"];
+    kernelModules = ["kvm-amd" "amd_pstate"];
     supportedFilesystems = ["btrfs" "ntfs"];
   };
   hardware = {
@@ -43,15 +43,15 @@
       0.0.0.0 log-upload-os.hoyoverse.com      # |
     '';
   };
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/9dd5237a-fab2-474d-9f0d-84f326ccefc9";
-      fsType = "btrfs";
-    };
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/F566-9676";
-      fsType = "vfat";
-    };
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/b505fb66-8b84-44ff-9d1f-789627d70fe7"; }
-    ];
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/9dd5237a-fab2-474d-9f0d-84f326ccefc9";
+    fsType = "btrfs";
+  };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/F566-9676";
+    fsType = "vfat";
+  };
+  swapDevices = [
+    {device = "/dev/disk/by-uuid/b505fb66-8b84-44ff-9d1f-789627d70fe7";}
+  ];
 }
