9640c9666d354e25be99c826a1d29e6f42505360
pagusupu <me@pagu.cafe>
2023-09-04 17:00:42 +1200

    cleanup

--- things/home/waybar.nix
+++ things/home/waybar.nix
@@ -7,44 +7,42 @@
   options.local.programs.waybar = {
     enable = lib.mkEnableOption "";
   };
-  config = lib.mkIf config.local.programs.waybar.enable {
-    programs.waybar = {
-      enable = true;
-      package = pkgs.waybar.overrideAttrs (oldAttrs: {
-        mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
-        postPatch = ''
-          substituteInPlace src/modules/wlr/workspace_manager.cpp --replace "zext_workspace_handle_v1_activate(workspace_handle_);" "const std::string command = \"${pkgs.hyprland}/bin/hyprctl dispatch workspace \" + name_; system(command.c_str());"
-        '';
-      });
-      settings = {
-        mainbar = {
-          layer = "top";
-          position = "top";
-          output = ["DP-3" "HDMI-A-1"];
-          modules-left = ["custom/icon" "clock"];
-          modules-center = ["hyprland/workspaces"];
-          modules-right = ["wireplumber" "bluetooth" "network" "custom/powermenu"];
-          "hyprland/workspaces" = {
-            format = "{icon}";
-            format-icons = {
-              "1" = "一";
-              "2" = "二";
-              "3" = "三";
-              "4" = "四";
-              "5" = "五";
-              "6" = "六";
-              "7" = "七";
-              "8" = "八";
+  config =
+    lib.mkIf config.local.programs.waybar.enable {
+      programs.waybar = {
+        enable = true;
+        package = pkgs.waybar.overrideAttrs (oldAttrs: {
+          mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+        });
+        settings = {
+          mainbar = {
+            layer = "top";
+            position = "top";
+            output = ["DP-3" "HDMI-A-1"];
+            modules-left = ["custom/icon" "clock"];
+            modules-center = ["hyprland/workspaces"];
+            modules-right = ["wireplumber" "bluetooth" "network" "custom/powermenu"];
+            "hyprland/workspaces" = {
+              format = "{icon}";
+              format-icons = {
+                "1" = "一";
+                "2" = "二";
+                "3" = "三";
+                "4" = "四";
+                "5" = "五";
+                "6" = "六";
+                "7" = "七";
+                "8" = "八";
+              };
+              persistent_workspaces = {
+                "*" = 4;
+              };
             };
-            persistent_workspaces = {
-              "*" = 4;
-            }; 
-          };
-          "clock" = {
-            format = "{:%I:%M %p}";
-            format-alt = "{:%I:%M %p, %A %B %d, %Y}";
-          };
-          "network" = {
+            "clock" = {
+              format = "{:%I:%M %p}";
+              format-alt = "{:%I:%M %p, %A %B %d, %Y}";
+            };
+            "network" = {
               format = "<span font_size='16pt'></span>";
               format-ethernet = "<span font_size='16pt'></span>";
               format-wifi = "<span font_size='16pt'></span>";
@@ -58,110 +56,110 @@
 {signalStrength}
 {bandwidthUpBits}
 {bandwidthDownBits}";
+            };
+            "wireplumber" = {
+              format = "<span font_size='16pt'></span>";
+              format-muted = "<span font_size='16pt'></span>";
+              tooltip-format = "{node_name} {volume}%";
+              on-click = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+              on-scroll-up = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+";
+              on-scroll-down = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-";
+            };
+            "custom/powermenu" = {
+              format = "<span font_size='16pt'></span>";
+              on-click = "sleep 0.1 && ~/Nix/things/scripts/powermenu.sh";
+              tooltip = false;
+            };
+            "bluetooth" = {
+              format = "<span font_size='16pt'></span>";
+              tooltip-format-connected = "{device_enumerate}";
+              tooltip-format-enumerate-connected = "{device_alias}: {device_address}";
+              tooltip-format-enumerated-connected-battery = "{device_alias} - {device_battery_percentage}%: {device_address}";
+              tooltip-format = "{controller_alias}: {status}";
+            };
+            "custom/icon" = {
+              format = "<span font_size='16pt'></span>";
+            };
+            height = 35;
+            spacing = 4;
+            margin-top = 10;
+            margin-bottom = 0;
+            margin-left = 10;
+            margin-right = 10;
           };
-          "wireplumber" = {
-            format = "<span font_size='16pt'></span>";
-	    format-muted = "<span font_size='16pt'></span>";
-            tooltip-format = "{node_name} {volume}%";
-            on-click = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
-            on-scroll-up = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+";
-            on-scroll-down = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-";
-          };
-          "custom/powermenu" = {
-            format = "<span font_size='16pt'></span>";
-            on-click = "sleep 0.1 && ~/Nix/things/scripts/powermenu.sh";
-            tooltip = false;
-          };
-          "bluetooth" = {
-             format = "<span font_size='16pt'></span>";
-             tooltip-format-connected = "{device_enumerate}";
-             tooltip-format-enumerate-connected = "{device_alias}: {device_address}";
-             tooltip-format-enumerated-connected-battery = "{device_alias} - {device_battery_percentage}%: {device_address}";
-             tooltip-format = "{controller_alias}: {status}";
-          };
-	  "custom/icon" = {
-	    format = "<span font_size='16pt'></span>";
-	  };
-          height = 35;
-          spacing = 4;
-          margin-top = 10;
-          margin-bottom = 0;
-          margin-left = 10;
-          margin-right = 10;
         };
+        style = ''
+          * {
+            border: none;
+            min-height: 0;
+            margin: 0;
+            padding: 0;
+            font-family: "Nunito", "Line Awesome Free", "Kosugi Maru";
+            font-size: 16px;
+          }
+          window#waybar {
+            background: #191919;
+            border-radius: 10px;
+          }
+          #workspaces {
+            margin: 4px;
+          }
+          #workspaces button {
+            padding-top: 0px;
+            padding-bottom: 0px;
+            padding-left: 2px;
+            padding-right: 2px;
+            color: #a0a0a0;
+            background-color: #393939;
+            border-radius: 6px;
+            margin: 2px;
+          }
+          #workspaces button.active {
+            background-color: #ac8aac;
+            color: #191919;
+            border-radius: 6px;
+          }
+          #workspaces button:hover {
+            background-color: #ac8aac;
+            color: #191919;
+            box-shadow: inherit;
+            text-shadow: inherit;
+            border-radius: 6px;
+          }
+          #clock {
+            color: #a0a0a0;
+            padding-top: 0px;
+            padding-bottom: 0px;
+            padding-right: 0px;
+            padding-left: 0px;
+          }
+          .modules-right {
+            color: #a0a0a0;
+            padding-left: 0px;
+            padding-right: 2px;
+            padding-top: 0px;
+            padding-bottom: 0px;
+          }
+          #custom-powermenu {
+            padding-right: 4px;
+          }
+          #network {
+            padding-right: 2px;
+          }
+          #custom-icon {
+            padding-left: 4px;
+            color: #a0a0a0;
+          }
+          tooltip {
+            background: #191919;
+            color: #a0a0a0;
+            border: 1px solid #a0a0a0;
+            border-radius: 6px;
+          }
+          tooltip label {
+            color: #a0a0a0;
+          }
+        '';
       };
-      style = ''
-        * {
-          border: none;
-          min-height: 0;
-          margin: 0;
-          padding: 0;
-          font-family: "Nunito", "Line Awesome Free", "Kosugi Maru";
-          font-size: 16px;
-        }
-        window#waybar {
-          background: #191919;
-          border-radius: 10px;
-        }
-	#workspaces {
-	  margin: 4px;
-	} 
-        #workspaces button {
-          padding-top: 0px;
-          padding-bottom: 0px;
-          padding-left: 2px;
-          padding-right: 2px;
-	  color: #a0a0a0;
-	  background-color: #393939;
-	  border-radius: 6px;
-	  margin: 2px;
-        }
-        #workspaces button.active {
-          background-color: #ac8aac;
-	  color: #191919;
-          border-radius: 6px;
-        }
-        #workspaces button:hover {
-          background-color: #ac8aac;
-	  color: #191919;
-          box-shadow: inherit;
-          text-shadow: inherit;
-          border-radius: 6px;
-        }
-        #clock {
-          color: #a0a0a0;
-          padding-top: 0px;
-          padding-bottom: 0px;
-          padding-right: 0px;
-          padding-left: 0px;
-        }
-	.modules-right {
-          color: #a0a0a0;
-          padding-left: 0px;
-          padding-right: 2px;
-          padding-top: 0px;
-          padding-bottom: 0px;
-        }
-        #custom-powermenu {
-	  padding-right: 4px;
-	}
-	#network {
-          padding-right: 2px;
-        }
-	#custom-icon {
-	  padding-left: 4px;
-	  color: #a0a0a0;
-	}
-        tooltip {
-          background: #191919;
-          color: #a0a0a0;
-          border: 1px solid #a0a0a0;
-          border-radius: 6px;
-        }
-        tooltip label {
-          color: #a0a0a0;
-        }
-      '';
     };
-  };
 }
