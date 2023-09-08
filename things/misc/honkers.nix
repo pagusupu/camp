cac2b5a8a7a8e189f5bafa96b1f35bb4670d129e
pagusupu <me@pagu.cafe>
2023-09-08 19:21:46 +1200

    update

--- things/misc/honkers.nix
+++ things/misc/honkers.nix
@@ -1,7 +1,7 @@
 let
   aagl-gtk-on-nix = import (builtins.fetchTarball {
     url = "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz";
-    sha256 = "0bl323dkil10z3kwf43vsiwn11i7argcj0a16z3bjmcjp4q890sc";
+    sha256 = "04yk09kmki2pbcm69cblnsbydq0faldc30k0rh2wpkgs5kkq0l51";
   });
 in {
   home.packages = [aagl-gtk-on-nix.the-honkers-railway-launcher];
