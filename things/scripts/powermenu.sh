013e2e639e64312fa49e07b715d64b6f02a6ca80
pagusupu <me@pagu.cafe>
2023-09-14 12:37:58 +1200

    powermenu update

--- things/scripts/powermenu.sh
+++ things/scripts/powermenu.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-op=$( echo -e " Poweroff\n Reboot\n Lock\n Logout" | tofi | awk '{print tolower($2)}' )
+op=$( echo -e " Poweroff\n Reboot\n Suspend\n Lock\n Logout" | tofi | awk '{print tolower($2)}' )
 
 case $op in 
         poweroff)
-		systemctl poweroff
-		;&
+		        systemctl poweroff
+		        ;&
         reboot)
-		systemctl reboot
-		;&
+		        systemctl reboot
+		        ;&
+        suspend)
+                systemctl suspend
+                ;&
         lock)
-		swaylock
+		        swaylock
                 ;;
         logout)
                 swaymsg exit
