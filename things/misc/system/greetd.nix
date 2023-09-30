f1929af770de90b0a9b986969224f5ecf77d8f62
pagusupu <me@pagu.cafe>
2023-09-30 13:17:31 +1300

    cleanup

--- things/misc/system/greetd.nix
+++ things/misc/system/greetd.nix
@@ -1,11 +1,6 @@
 {pkgs, ...}: {
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet -r -t --asterisks -c sway";
-        user = "pagu";
-      };
-    };
+  services.greetd.settings.default_session = {
+    command = "${pkgs.greetd.tuigreet}/bin/tuigreet -r -t --asterisks -c sway";
+    user = "pagu";
   };
 }
