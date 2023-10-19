34db57cc35077b0d3fb9d5b8f1e2992e3acd4ad1
pagusupu <me@pagu.cafe>
2023-10-20 08:51:07 +1300

    a bit more cleanup

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     alejandra.url = "github:kamadorueda/alejandra/3.0.0";
-    nix-index-database.url = "github:Mic92/nix-index-database";
     nixvim.url = "github:nix-community/nixvim";
   };
   outputs = {nixpkgs, ...} @ inputs: {
