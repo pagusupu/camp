b7b046def06e7098e76c9d498a38fc336a66e9cb
pagusupu <me@pagu.cafe>
2024-04-28 14:52:17 +1200

    remove etesync

--- misc/secrets/secrets.nix
+++ misc/secrets/secrets.nix
@@ -2,7 +2,6 @@ let
   server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKj709k07PEtMHhT9Leb1pVkS2kduiyogmyXqNmLRgfp";
   desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMGwCFQYJB+4nhIqktQwJemynSOEP/sobnV2vESSY3tk";
 in {
-  "etebase.age".publicKeys = [server];
   "mail.age".publicKeys = [server];
   "navi-fm.age".publicKeys = [server];
   "nextcloud.age".publicKeys = [server];
