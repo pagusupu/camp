75c21ed0b07a7e672fbeac5e4ca12c12a7d753f2
pagusupu <me@pagu.cafe>
2024-05-07 16:10:58 +1200

    starship

--- modules/programs/cli/starship.nix
+++ modules/programs/cli/starship.nix
@@ -9,26 +9,28 @@
       enable = true;
       settings = {
         add_newline = false;
-        format = "$hostname\$directory\$git_branch\$git_status\$cmd_duration\$line_break\$character";
+        format = ''
+          [\[$username[@](bold cyan)$hostname\] $directory$git_branch$git_status]($style)
+          [](bold purple) $character
+        '';
         character = {
-          error_symbol = "[](bold red)";
-          success_symbol = "[](bold green)";
-          vimcmd_symbol = "[](bold green)";
-          vimcmd_replace_one_symbol = "[](bold orange)";
-          vimcmd_replace_symbol = "[](bold orange)";
-          vimcmd_visual_symbol = "[](bold yellow)";
+          error_symbol = "[󰜥 ](bold red)";
+          success_symbol = "[󰜥 ](bold blue)";
         };
         directory = {
           home_symbol = " ";
           read_only = " ";
-          style = "bold purple";
-          truncation_length = 5;
+          style = "bold green";
         };
         hostname = {
-          format = "[$hostname](bold purple) | ";
+          format = "[$hostname](bold yellow)[$ssh_symbol](bold red)";
+          ssh_symbol = "<ssh>";
           ssh_only = false;
         };
-        git_status.deleted = "x";
+        username = {
+          format = "[$user]($style)";
+          show_always = true;
+        };
       };
     };
   };
