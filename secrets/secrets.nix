e9ca181006f577effea3bcf84760222735b00993
pagusupu <me@pagu.cafe>
2024-09-19 15:13:32 +1200

    fix secrets again

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
-  aoi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKj709k07PEtMHhT9Leb1pVkS2kduiyogmyXqNmLRgfp";
-  rin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMGwCFQYJB+4nhIqktQwJemynSOEP/sobnV2vESSY3tk";
+  aoi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIExuEEnRUnoo1qZVnvLUtvXqCcBd7DcDJkohVCg0Qbij";
+  rin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIcZJ0IEzuvGNglNevP/pSpHNYd+iJwrpRO2yK8mg4lt";
 in {
   "nextcloud.age".publicKeys = [aoi];
   "tailscale.age".publicKeys = [aoi];
