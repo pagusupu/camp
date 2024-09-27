c0b15cd6dc865ce312635e7d441c62834d7aa10d
pagusupu <me@pagu.cafe>
2024-09-27 22:07:25 +1200

    rekey again

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   aoi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIExuEEnRUnoo1qZVnvLUtvXqCcBd7DcDJkohVCg0Qbij";
-  ena = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEGxD4XtJUHp+wFASN6AQyda+p4Ry6nLTmG7kGO3szc/";
+  ena = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDM4qA5VHz8pRZMmEZk06ber5mm3apBexIwkc5pIlQvE";
   rin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIcZJ0IEzuvGNglNevP/pSpHNYd+iJwrpRO2yK8mg4lt";
 in {
   "etebase.age".publicKeys = [aoi];
